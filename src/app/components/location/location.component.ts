e2b1ccf037fafbc60004e135ff3968631bd97806
AntonioAldamaG <22041328@itdurango.edu.mx>
2025-05-21 14:34:28 -0600

    mapa si

--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -1,13 +1,14 @@
 import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { BaseServiceComponent } from '../base-service/base-service.component';
 import { CommonModule } from '@angular/common';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.css'],
   standalone: true,
-  imports: [CommonModule, BaseServiceComponent],
+  imports: [CommonModule, BaseServiceComponent, NavBarComponent],
 })
 export class LocationComponent implements AfterViewInit {
   @ViewChild('baseServiceRef') baseServiceComponent!: BaseServiceComponent;
