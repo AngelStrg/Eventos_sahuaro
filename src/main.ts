574b15fb85252a64d7085b8486e86309681508ba
AntonioAldamaG <22041328@itdurango.edu.mx>
2025-06-08 19:33:56 -0600

    si

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)]
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "eventos-sahuaro", appId: "1:927912995819:web:87ef942c7a02df24b3c7ae", storageBucket: "eventos-sahuaro.firebasestorage.app", apiKey: "AIzaSyDvjLsLxS2EYq-oCl5fh_bWx1lrb54478M", authDomain: "eventos-sahuaro.firebaseapp.com", messagingSenderId: "927912995819" })), provideFirestore(() => getFirestore())]
 }
 ).catch((err) => console.error(err));
 
