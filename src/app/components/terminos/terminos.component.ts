dd72838584b61cbefa3446211e3e61394887641e
AngelStrg <zaidg6414@gmail.com>
2025-06-11 12:51:04 -0600

    zi

--- src/app/components/terminos/terminos.component.ts
+++ src/app/components/terminos/terminos.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavBarComponent } from "../components/nav-bar/nav-bar.component";
-import { FooterComponent } from "../components/footer/footer.component";
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { FooterComponent } from "../footer/footer.component";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
